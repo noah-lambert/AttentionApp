e61f9b88f6402887913f923b2b7c2c87ae9d3373
noah-lambert <61210253+noah-lambert@users.noreply.github.com>
2021-10-18 12:45:08 -0400

    Initial Commit

---
+++ Shared/AttentionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttentionAppApp.swift
+//  Shared
+//
+//  Created by Noah  Lambert  on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct AttentionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
